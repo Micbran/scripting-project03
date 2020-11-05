b5fb1d424db94e1eb516f3751fbd98e7b5d3ddd0
Micbran <micbran22@gmail.com>
2020-11-05 17:39:24 -0600

    Basic shit + bad code that does what I want.

---
+++ Assets/Scripts/GrappleHandler.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GrappleInput))]
+public class GrappleHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject fistObject = null;
+    [SerializeField] private Transform playerFistHome = null;
+
+    [SerializeField] private float maxDistance = 20f;
+    [SerializeField] private float grappleFlySpeed = 10f;
+    private Camera mainCamera = null;
+
+    private GrappleInput inputHandler = null;
+
+    private bool grappleRetracted = true;
+    private bool isGrappling = false;
+    private Coroutine coroutRef;
+
+    private int tester = 1;
+
+    private RaycastHit grapplePoint;
+
+    private void Awake()
+    {
+        inputHandler = this.GetComponent<GrappleInput>();
+        mainCamera = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        inputHandler.GrapplePressed += OnGrapplePressed;
+        inputHandler.GrappleReleased += OnGrappleReleased;
+    }
+
+    private void OnDisable()
+    {
+        inputHandler.GrapplePressed -= OnGrapplePressed;
+        inputHandler.GrappleReleased -= OnGrappleReleased;
+    }
+
+    private void OnGrapplePressed()
+    {
+        grappleRetracted = false;
+        Debug.Log("Grapple fired!");
+        fistObject.SetActive(true);
+        Ray toBeRaycasted = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(toBeRaycasted, out grapplePoint, maxDistance))
+        {
+            fistObject.transform.LookAt(grapplePoint.point);
+            coroutRef = StartCoroutine(Grappling());
+        }
+
+    }
+
+    private void OnGrappleReleased()
+    {
+        grappleRetracted = true;
+        Debug.Log("Grapple retracting...");
+        fistObject.transform.position = playerFistHome.transform.position;
+        StopCoroutine(coroutRef);
+    }
+
+    private IEnumerator Grappling()
+    {
+        while(true)
+        {
+            Vector3 grappleDirection = grapplePoint.point - fistObject.transform.position;
+            if(grappleDirection.magnitude >= 0.5f)
+                fistObject.transform.position = fistObject.transform.position + grappleDirection.normalized * grappleFlySpeed;
+            Debug.Log("Test: " + tester++);
+            yield return null;
+        }
+    }
+
+}
---
+++ Assets/Scripts/GrappleInput.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+public class GrappleInput : MonoBehaviour
+{
+    public event Action GrapplePressed = delegate { };
+    public event Action GrappleReleased = delegate { };
+
+
+    private void Update()
+    {
+        DetectGrappleInput();
+    }
+
+    private void DetectGrappleInput()
+    {
+        if(Input.GetMouseButtonDown((int)MouseButton.RightClick))
+        {
+            GrapplePressed.Invoke();
+        }
+        else if(Input.GetMouseButtonUp((int)MouseButton.RightClick))
+        {
+            GrappleReleased.Invoke();
+        }
+    }
+}
+
+public enum MouseButton
+{
+    LeftClick = 0,
+    RightClick = 1,
+    MiddleClick = 2
+}
